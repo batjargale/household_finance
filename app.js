cd7e643560484b8a3b1f454a93f2a2dafb6acf5f
batjargale <batjargale@gmail.com>
2022-08-22 14:33:42 +0800

    UI controller deer JS negdsen classuudiig oruulah

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 // Дэлгэцтэй ажиллах контроллер
-var uiController = (function () {})();
+var uiController = (function () {
+  // Ирээдүйд CSS дизайнер өөрчлөлт хийвэл өөрчлөлт хийхэд хялбар, алдаа гарахгүй
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDesc: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        desc: document.querySelector(DOMstrings.inputDesc).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 // Санхүүтэй ажиллах контроллер
 var financeController = (function () {})();
 
 // Програмын холбогч контроллер
 var appController = (function (uiController, financeController) {
+  var DOM = uiController.getDOMstrings(); //UI aas css class uudiig duudaj bna
   var ctrlAddItem = function () {
     //1. Оруулах өгөгдлийг дэлгэцнээс олж авна.
-    console.log("Дэлгэцэнд мэдээлэл орлоо");
+    console.log(uiController.getInput());
     //2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж, тэнд хадгална.
     //3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт дамжуулж, тэнд хадгална.
     //4.Төсвийг тооцоолно.
     //5.Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
     // console.log("click");
   };
-  document.querySelector(".add__btn").addEventListener("click", function () {
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
     ctrlAddItem();
   });
 
